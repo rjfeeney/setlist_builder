12a0c38029facfbdcedf256e7173ffcb0cb1481e
root <root@DESKTOP-7BECLG9.localdomain>
2025-06-23 15:59:17 -0400

    Dockerfile and Sample Database

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM golang:1.21-alpine AS builder
+WORKDIR /app
+COPY go.mod go.sum ./
+RUN go mod download
+COPY . .
+RUN CGO_ENABLED=1 GOOS=linux go build -a -installsuffix cgo -o setlist_builder .
+
+FROM gcr.io/distroless/static-debian11
+WORKDIR /app
+COPY --from=builder /app/setlist_builder .
+CMD ["./setlist_builder"]
---
+++ init.sql
@@ -0,0 +1,36 @@
+CREATE TABLE tracks (
+    name TEXT NOT NULL,
+    artist TEXT NOT NULL,
+    genre TEXT[],
+    duration_in_seconds INT NOT NULL,
+    year TEXT NOT NULL,
+    explicit BOOL NOT NULL DEFAULT false,
+    bpm INT NOT NULL DEFAULT 0,
+    original_key TEXT NOT NULL DEFAULT '',
+    CONSTRAINT PK_name_artist PRIMARY KEY(name,artist)
+);
+
+CREATE TABLE working (
+    name TEXT NOT NULL,
+    artist TEXT NOT NULL,
+    genre TEXT[],
+    duration_in_seconds INT NOT NULL,
+    year TEXT NOT NULL,
+    explicit BOOL NOT NULL DEFAULT false,
+    bpm INT NOT NULL,
+    original_key TEXT NOT NULL,
+    singer TEXT,
+    singer_key TEXT,
+    CONSTRAINT PK_working PRIMARY KEY(name,artist)
+);
+
+CREATE TABLE singers (
+    song TEXT NOT NULL,
+    artist TEXT NOT NULL,
+    singer TEXT NOT NULL,
+    key TEXT NOT NULL,
+    CONSTRAINT PK_singers PRIMARY KEY(song, artist, singer),
+    CONSTRAINT FK_singers_tracks FOREIGN KEY (song, artist)
+        REFERENCES tracks(name, artist)
+        ON DELETE CASCADE
+);
