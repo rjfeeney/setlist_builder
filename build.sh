9d5dcd447adefbb775acd97c253297c570396c8c
Riley Feeney <rileyfeeney7@gmail.com>
2025-07-25 11:44:21 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -e
 mkdir -p output
-docker build -f Dockerfile.build -t setlist-builder-builder .
+docker build --build-arg GOOS=linux --build-arg GOARCH=amd64 -f Dockerfile.build -t setlist-builder-builder .
 docker run --rm -v "$PWD/output":/app/output setlist-builder-builder
 echo "Build complete. Binary is in ./output/"
