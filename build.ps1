3bcaf8ce49f2453dbb0664eb3f82a7b7bb37d3af
Riley Feeney <rileyfeeney7@gmail.com>
2025-07-25 12:30:32 -0400

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -3,6 +3,7 @@ if (-not (Test-Path -Path $outputPath)) {
     New-Item -ItemType Directory -Path $outputPath | Out-Null
 }
 docker build -t setlist_builder_build_temp -f Dockerfile.build .
+$fullOutputPath = (Resolve-Path $outputPath).Path
 $containerId = docker create setlist_builder_build_temp
-docker cp "$containerId:/app/output/setlist" "$outputPath/setlist.exe"
+docker cp "$containerId:/app/output/setlist" "$fullOutputPath\setlist.exe"
 docker rm $containerId
