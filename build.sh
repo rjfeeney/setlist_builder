c214dfbd001ee1c8a769e18341ae30a661905f10
Riley Feeney <rileyfeeney7@gmail.com>
2025-07-25 11:48:19 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
 set -e
 mkdir -p output
-docker build --build-arg GOOS=linux --build-arg GOARCH=amd64 -f Dockerfile.build -t setlist-builder-builder .
+UNAME=$(uname | tr '[:upper:]' '[:lower:]')
+if [[ "$UNAME" == "darwin" ]]; then
+  GOOS="darwin"
+elif [[ "$UNAME" == "linux" ]]; then
+  GOOS="linux"
+else
+  echo "Unsupported OS: $UNAME"
+  exit 1
+fi
+GOARCH="amd64"
+echo "Building for OS=$GOOS ARCH=$GOARCH"
+docker build --build-arg GOOS=$GOOS --build-arg GOARCH=$GOARCH -f Dockerfile.build -t setlist-builder-builder .
 docker run --rm -v "$PWD/output":/app/output setlist-builder-builder
 echo "Build complete. Binary is in ./output/"
