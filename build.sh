6c1fac621dd4425acd9cdd739694112cebb8c8c0
Riley Feeney <rileyfeeney7@gmail.com>
2025-07-25 13:05:27 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 outputPath="./output"
+binaryName="setlist"
+imageName="setlist_builder_build_temp"
 mkdir -p "$outputPath"
-docker build --build-arg GOOS=linux --build-arg GOARCH=amd64 -f Dockerfile.build -t setlist_builder_build_temp .
-containerId=$(docker create setlist_builder_build_temp)
-docker cp "$containerId:/app/output/setlist_builder" "$fullOutputPath\setlist.exe"
+docker build --build-arg GOOS=linux --build-arg GOARCH=amd64 -f Dockerfile.build -t $imageName .
+containerId=$(docker create $imageName)
+docker cp "$containerId:/app/setlist_builder" "$outputPath/$binaryName"
 docker rm "$containerId"
-echo "Build complete! The binary is at $outputPath/setlist"
-
+echo "✅ Build complete! The binary is at $outputPath/$binaryName"
