d3d8cb42338c08dd410b558f33f10c8a50c8082a
Riley Feeney <rileyfeeney7@gmail.com>
2025-07-25 12:29:29 -0400

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -4,7 +4,5 @@ if (-not (Test-Path -Path $outputPath)) {
 }
 docker build -t setlist_builder_build_temp -f Dockerfile.build .
 $containerId = docker create setlist_builder_build_temp
-docker start -ai $containerId
-docker cp "${containerId}:/app/output/setlist" "$outputPath/setlist.exe"
+docker cp "$containerId:/app/output/setlist" "$outputPath/setlist.exe"
 docker rm $containerId
-Write-Host "Build complete! The binary is at .\$outputPath\setlist.exe"
